3435d85bcb4cdd6111d03c131394a321e22a8418
JEEBIS27 <jeebis.iox@gmail.com>
2026-04-15 09:43:28 +0900

    fix a path of dictionaries

--- phonetic/system.py
+++ phonetic/system.py
@@ -100,7 +100,7 @@ KEYMAPS = {
         'arpeggiate' : 'return',
         }
 }
-DICTIONARIES_ROOT = 'asset:phonetic:dictionaries/default'
+DICTIONARIES_ROOT = 'asset:phonetic:dictionaries'
 DEFAULT_DICTIONARIES = ('phonetic-user.json','phonetic-commands.json','phonetic-brief.json','phonetic-base.json','jeff-phrasing.py')
 
 
